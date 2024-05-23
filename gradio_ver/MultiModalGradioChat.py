0415afa8a46ad2c9c5ee11d2e528670b75309bc0
AnhNguyen20695 <felixnguyen1520@gmail.com>
2024-05-23 15:41:11 +1000

    fix

--- gradio_ver/MultiModalGradioChat.py
+++ gradio_ver/MultiModalGradioChat.py
@@ -6,9 +6,10 @@ import os
 import requests
 from config import *
 import numpy as np
-from scipy.io.wavfile import write
 
-  
+import azure.cognitiveservices.speech as speechsdk
+
+
 load_dotenv()  
 
 #set max chat history to keep
@@ -16,6 +17,7 @@ max_items = 16
 
 # Initialize an empty list for the conversation history with max len
 conversation_history = []
+chat_conversation_history = []
 
 
 #Set Headers
@@ -27,7 +29,8 @@ headers = {
 
 
 # Add the system message to the conversation history , Cutomize the System message to your needs
-system_message = {
+#Maintain the conversation_history 
+conversation_history.append({
     "role": "system",
     "content": [
         {
@@ -35,9 +38,11 @@ system_message = {
             "text": "You are an AI assistant that helps people find information.Make the URL as markdown hyper links for rendering as hyperlinks example [Gradio Website][1][1]: https://www.gradio.app/"
         }
     ]
-}
-#Maintain the conversation_history 
-conversation_history.append(system_message)
+})
+chat_conversation_history.append({
+    "role": "system",
+    "content": "You are an AI assistant that helps people find information.Make the URL as markdown hyper links for rendering as hyperlinks example [Gradio Website][1][1]: https://www.gradio.app/"
+})
 
 #RAG Pattern Index details where Images are embedded for Retrieval
 dataSources = [
@@ -46,13 +51,46 @@ dataSources = [
                 "parameters": {
                     "endpoint": AZ_SEARCH_ENDPOINT,
                     "key": AZ_SEARCH_KEY,
-                    "indexName":AZ_SEARCH_INDEX,
+                    "indexName":AZ_IMAGE_SEARCH_INDEX,
+                    "semanticConfiguration": "default",
+                    "queryType": "semantic",
+                    "in_scope": True,
                     "fieldsMapping": {
                                     "vectorFields": "image_vector"
                                 },
                 }
             }
         ]
+
+text_datasources = [{
+    "type": "AzureCognitiveSearch",
+    "parameters": {
+        "endpoint": AZ_SEARCH_ENDPOINT,
+        "key": AZ_SEARCH_KEY,
+        "indexName": AZ_DOC_SEARCH_INDEX,
+        "semantic_configuration": "default",
+        "query_type": "vectorSemanticHybrid",
+        "fields_mapping": {
+            "content_fields_separator": "\n",
+            "content_fields": [
+                "content"
+            ],
+            "filepath_field": "filepath",
+            "title_field": "title",
+            "url_field": "url",
+            "vector_fields": [
+                "contentVector"
+            ]
+        },
+        "in_scope": True,
+        "strictness": 3,
+        "top_n_documents": 5,
+        "embedding_dependency": {
+            "type": "deployment_name",
+            "deployment_name": AZ_EMBEDDING_DEPLOYMENT
+        },
+    }
+}]
 
 
 #current_dir the avatar image needs to be placed in current_dir
@@ -75,13 +113,41 @@ def convertImageToBase64(image_path):
         encoded_string = base64.b64encode(img.read())
     return encoded_string.decode('ascii')
 
-def transcribe(audio, state=""):
-    print(audio)
-    # text = pipe(audio)["text"]
-    text = "Success"
-    state += text + " "
+def recognize_from_wav(filename):
+    # This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+    speech_config = speechsdk.SpeechConfig(subscription=AZ_SPEECH_TEXT_KEY,
+                                           region=AZ_SPEECH_TEXT_REGION)
+    speech_config.speech_recognition_language=AZ_SPEECH_LANGUAGE
 
-    return state, state
+    audio_config = speechsdk.audio.AudioConfig(filename=filename)
+    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
+
+    speech_recognition_result = speech_recognizer.recognize_once_async().get()
+
+    if speech_recognition_result.reason == speechsdk.ResultReason.RecognizedSpeech:
+        # print("Recognized: {}".format(speech_recognition_result.text))
+        return "", speech_recognition_result.text
+    elif speech_recognition_result.reason == speechsdk.ResultReason.NoMatch:
+        return f"No speech could be recognized: {speech_recognition_result.no_match_details}", ""
+    elif speech_recognition_result.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = speech_recognition_result.cancellation_details
+        # print("Speech Recognition canceled: {}".format(cancellation_details.reason))
+        if cancellation_details.reason == speechsdk.CancellationReason.Error:
+            print("Error details: {}".format(cancellation_details.error_details))
+            print("Did you set the speech resource key and region values?")
+            return f"Error details: {cancellation_details.error_details}", ""
+
+def transcribe(audio, status_box):
+    if status_box == "available":
+        # Switch play
+        error, text = recognize_from_wav(audio)
+        status = "recording"
+        print("Error: ",error)
+    else:
+        # Clear previous record
+        text = ""
+        status = "available"
+    return status, text
 
 # Function that takes User Inputs and displays it on ChatUI and also maintains the history for chatcompletion
 def buildHistoryForUiAndChatCompletion(history,txt,img):
@@ -99,6 +165,10 @@ def buildHistoryForUiAndChatCompletion(history,txt,img):
                 ]
             }
         conversation_history.append(user_message)
+        chat_conversation_history.append({
+            "role": "user",
+            "content": txt
+        })
         return history
     #if user enters image and text
     base64 = convertImageToBase64(img)
@@ -122,61 +192,72 @@ def buildHistoryForUiAndChatCompletion(history,txt,img):
     }
     #append the user message to chat api pay load
     conversation_history.append(user_message)
+    chat_conversation_history.append({
+            "role": "user",
+            "content": txt
+        })
     return history
 
 # Function that takes User Inputs, generates Response and displays on Chat UI
 def call_AzureOpenAI_Vision_RAG_API(history,text,img):
-    body = {
-        "dataSources": dataSources,
-        "messages": conversation_history,
-        "max_tokens": 100,
-        "temperature": 0,
-        "top_p": 1
-    }
-    
-    vision_gpt4_endpoint = GPT4V_ENDPOINT+"openai/deployments/"+GPT4V_DEPLOYMENT_NAME+"/extensions/chat/completions?api-version="+GPT4V_VERSION
-    chat_gpt3_endpoint = ""
+    if not img:
+        print("Chat conv history: ",chat_conversation_history)
+        body = {
+            "dataSources": text_datasources,
+            "messages": chat_conversation_history,
+            "max_tokens": 100,
+            "temperature": 0,
+            "top_p": 1
+        }
+        
+        endpoint = GPT35_ENDPOINT+"openai/deployments/"+GPT35_DEPLOYMENT_NAME+"/extensions/chat/completions?api-version="+GPT35_VERSION
+    else:
+        body = {
+            "dataSources": dataSources,
+            "messages": conversation_history,
+            "max_tokens": 100,
+            "temperature": 0,
+            "top_p": 1
+        }
+        
+        endpoint = GPT4V_ENDPOINT+"openai/deployments/"+GPT4V_DEPLOYMENT_NAME+"/extensions/chat/completions?api-version="+GPT4V_VERSION
 
     #post the API request
     print("Requesting ",endpoint)
     response = requests.post(endpoint, headers=headers, json=body)
     print(response.json())
     #get llm reponse
-    content = response.json()['choices'][0]['message']['content']
+    if not img:
+        messages = response.json()['choices'][0]["messages"]
+        txt = ""
+        content = ""
+        for message in messages:
+            if message["role"] == "assistant":
+                txt = message["content"]
+                content = message["content"]
+                break
+        assistant_message = {
+            "role": "assistant",
+            "content": txt
+        }
+    else:
+        assistant_message = {
+            "role": "assistant",
+            "content": [
+                {
+                    "type": "text",
+                    "text": response.json()['choices'][0]['message']['content']
+                }
+            ]
+        }
+        content = response.json()['choices'][0]['message']['content']
  
     #llm response added to history
-    assistant_message = {
-        "role": "assistant",
-        "content": [
-            {
-                "type": "text",
-                "text": content
-            }
-        ]
-    }
     conversation_history.append(assistant_message)
+    chat_conversation_history.append(assistant_message)
     history += [(None,content)]
     #conversation_history = keep_latest_n_items(conversation_history, 10)
     return history 
-    
-def click_js():
-    return """function audioRecord() {
-    var xPathRes = document.evaluate ('//*[contains(@class, "record")]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null); 
-    xPathRes.singleNodeValue.click();}"""
-
-
-def action(btn):
-    """Changes button text on click"""
-    if btn == 'Speak': return 'Stop'
-    else: return 'Speak'
-
-
-def check_btn(btn):
-    """Checks for correct button text before invoking transcribe()"""
-    if btn != 'Speak': raise Exception('Recording...')
-
-def transcribe():
-    return 'Success'
 
 # Interface Code
 with gr.Blocks(theme=gr.themes.Soft(primary_hue="green", secondary_hue="red")) as app:
@@ -198,22 +279,24 @@ with gr.Blocks(theme=gr.themes.Soft(primary_hue="green", secondary_hue="red")) a
                     placeholder="Enter text and press enter",
                     container=False,
                 )
+            status_box = gr.Textbox(value="available",visible=False)
         with gr.Column():
-            audio_btn = gr.Button('Speak')
-            clear = gr.Button("Clear")
-            audio_box = gr.Audio(label="Audio", sources="microphone", type="filepath", elem_id='audio')
+            audio_box = gr.Audio(label="Audio",
+                                 sources="microphone",
+                                 type="filepath",
+                                 elem_id='audio',
+                                 format='wav',
+                                 max_length=10)
 
-            audio_btn.click(fn=action, inputs=audio_btn, outputs=audio_btn).\
-                    then(fn=lambda: None, js=click_js()).\
-                    then(fn=check_btn, inputs=audio_btn).\
-                    success(fn=transcribe, outputs=text_box)
+            audio_box.change(transcribe,
+                             inputs=[audio_box, status_box],
+                             outputs=[status_box, text_box])
 
-            clear.click(lambda: None, None, text_box, queue=False)
 
     #btnupd = gr.UploadButton("📁", file_types=["image", "video", "audio"],type="filepath")
     btn = gr.Button("Submit")
     clicked = btn.click(buildHistoryForUiAndChatCompletion,
-                        [chatbot,text_box,image_box,audio_box],
+                        [chatbot,text_box,image_box],
                         chatbot
                         ).then(call_AzureOpenAI_Vision_RAG_API,
                                 [chatbot,text_box,image_box],
--- gradio_ver/config.py
+++ gradio_ver/config.py
@@ -4,8 +4,36 @@ GPT4V_ENDPOINT = 'https://sit788-felix-openai.openai.azure.com/'
 GPT4V_DEPLOYMENT_NAME = 'sit788-felix-model'
 GPT4V_VERSION = '2023-07-01-preview'
 
+GPT35_KEY='a3cfaa3c01314b63a56eb3e69ee8ae4c'
+location = 'australiaeast'
+GPT35_ENDPOINT = 'https://sit788-felix-openai.openai.azure.com/'
+GPT35_DEPLOYMENT_NAME = 'sit788-felix-text'
+GPT35_VERSION = '2023-07-01-preview'
+
+AZ_EMBEDDING_DEPLOYMENT = "sit788-felix-embedding"
 
 AZ_SEARCH_SERVICE = 'sit788-felix-cognitive-search'
 AZ_SEARCH_ENDPOINT = "https://{}.search.windows.net/".format(AZ_SEARCH_SERVICE)
-AZ_SEARCH_INDEX = 'bot_index'
+AZ_IMAGE_SEARCH_INDEX = 'bot_index'
+AZ_DOC_SEARCH_INDEX = 'bot-doc-index'
 AZ_SEARCH_KEY = 'z256AnkW7csSktPZibZtVt7StKgXzuMm9jlkiGvwBSAzSeDJAtAQ'
+
+AZ_SPEECH_TEXT_ENDPOINT = 'https://australiaeast.api.cognitive.microsoft.com/'
+AZ_SPEECH_TEXT_REGION = 'australiaeast'
+AZ_SPEECH_TEXT_KEY = '6ed7394726fc488a95e39a1415e569b6'
+AZ_SPEECH_LANGUAGE = "en-US"
+
+AZ_SUBSCRIPTION_ID = "c17a0f1e-eff5-4d9a-b56b-20d006070ed2"
+AZ_STORAGE_ACCOUNT = "sit788felixstorage"
+AZ_STORAGE_KEY = "ap3Z1hyNL31LBVVMKeylrbsvibVYQjNwqQihFa4KONhPFEOylM21vjXOayTGrAx96tYr2TtqFQ0F+ASt/+Crrw=="
+AZ_STORAGE_CONNECTION_STRING = "ap3Z1hyNL31LBVVMKeylrbsvibVYQjNwqQihFa4KONhPFEOylM21vjXOayTGrAx96tYr2TtqFQ0F+ASt/+Crrw=="
+AZ_STORAGE_CONTAINER_NAME = "bot-data-private"
+AZ_STORAGE_RESOURCE_GROUP = "SIT788-Felix"
+
+AZ_OPENAI_KEY = "a3cfaa3c01314b63a56eb3e69ee8ae4c"
+AZ_OPENAI_SERVICE = "https://sit788-felix-openai.openai.azure.com/"
+AZ_OPENAI_EMBEDDING_MODEL = "text-embedding-ada-002"
+AZ_OPENAI_EMBEDDING_DEPLOYMENT = "sit788-felix-embedding"
+
+AZ_DOC_INTELLIGENCE_SERVICE = "https://sit788-felix-doc-int.cognitiveservices.azure.com/"
+AZ_DOC_INTELLIGENCE_KEY = "2f7a3f7521484dc68d4a4f94b10914ab"
